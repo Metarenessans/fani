bfd8a374f3868dcd7db4153816cbd7f2c8195773
persiklover <dannniska@gmail.com>
2021-03-16 12:25:48 +0300

    16.03.2021

--- common/components/numeric-input/index.test.js
+++ common/components/numeric-input/index.test.js
@@ -6,4 +6,18 @@ import NumericInput from "./index"
 test("renders correctly", () => {
   const { container } = render(<NumericInput/>);
   expect(container).toBeTruthy();
+
+  const inputElement = container.querySelector("input");
+  // console.dir(inputElement);
+
+  describe("По дефолту инпут пустой", () => {
+    expect(inputElement.value).toEqual("");
+  });
+
+  describe("В инпут помещается значение из props.defaultValue", () => {
+    const { container } = render(<NumericInput defaultValue={0} />);
+    const inputElement = container.querySelector("input");
+    expect(inputElement.value).toEqual("0");
+  });
+
 })
