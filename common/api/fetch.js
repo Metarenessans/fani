69cab1f2ffee9f62de7859092136b767b2d3eeab
persiklover <dannniska@gmail.com>
2021-03-17 14:35:13 +0300

    Более универсальный запрос к серверу

--- common/api/fetch.js
+++ common/api/fetch.js
@@ -4,7 +4,7 @@ export default function fetch(url = "", method = "GET", data = {}) {
   console.log(`Sending ${url} request...`);
   return new Promise((resolve, reject) => {
     ajax({
-      url: `https://fani144.ru/local/php_interface/s1/ajax/?method=${url}`,
+      url: `/local/php_interface/s1/ajax/?method=${url}`,
       method,
       data,
       success: response => {
