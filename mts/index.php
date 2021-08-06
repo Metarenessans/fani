6bfb33b77a06f635ab4ef12f142401536014db65
persiklover <dannniska@gmail.com>
2021-08-06 23:18:41 +0300

    revert webpack config

---
+++ mts/index.php
@@ -0,0 +1,17 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("МТС");
+?>
+  <!-- Include CSS -->
+  <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+  <noscript>You need to enable JavaScript to run this app</noscript>
+  <div id="root"></div>
+  
+  <script type="module" src="public/index-es6.js"></script>
+  <script nomodule src="public/index.js"></script>
+
+  <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+</body>
+</html>
