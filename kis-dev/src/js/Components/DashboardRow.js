44f9ad500af49407d9fee904a5ff110c911c067d
Metarenessans <zero.neitrino@gmail.com>
2021-08-08 06:59:59 +0300

    -

--- kis-dev/src/js/Components/DashboardRow.js
+++ kis-dev/src/js/Components/DashboardRow.js
@@ -224,20 +224,20 @@ export default class DashboardRow extends React.Component {
 
         if (i == 2) {
           monthBalance = arr[i - 1].monthBalance + (rentIncome + monthAppend) - (monthPay + monthOutcome);
-          lostProfit = arr[i - 1].lostProfit + (firstPay - monthBalance) / 12 * profitPercent;
-          endSum = monthBalance + -lostProfit;
+          lostProfit   = arr[i - 1].lostProfit + (firstPay - monthBalance) / 12 * profitPercent;
+          endSum       = monthBalance + -lostProfit;
         }
 
         if (i > 2) {
           monthBalance = arr[i - 1].monthBalance + (rentIncome + monthAppend) - (monthPay + monthOutcome);
-          lostProfit = arr[i - 1].lostProfit + (firstPay - monthBalance) / 12 * profitPercent;
-          endSum = monthBalance + -lostProfit;
+          lostProfit   = arr[i - 1].lostProfit + (firstPay - monthBalance) / 12 * profitPercent;
+          endSum       = monthBalance + -lostProfit;
         }
 
         arr[i] = {
-          monthBalance: round(monthBalance),
-          lostProfit: round(lostProfit),
-          endSum: round(endSum)
+          monthBalance : round(monthBalance),
+          lostProfit   : round(lostProfit),
+          endSum       : round(endSum)
         }
       }
 
