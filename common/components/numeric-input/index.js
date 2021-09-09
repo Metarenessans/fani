19170ef2cd83fa2750709e935e78e92e109bbe31
persiklover <dannniska@gmail.com>
2021-09-09 20:38:42 +0300

    Минус в NumericInput на мобилках теперь виден всегда

--- common/components/numeric-input/index.js
+++ common/components/numeric-input/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Input, Tooltip } from 'antd/es'
+import clsx from 'clsx';
 import './style.scss'
 
-let isIPhone = /iPhone/i.test(navigator.userAgent);
+const isMobile = innerWidth <= 768;
 
 export default class NumericInput extends React.Component {
   constructor(props) {
@@ -32,7 +33,11 @@ export default class NumericInput extends React.Component {
 
   get unsigned() {
     return this.props.unsigned == "true"
-  } 
+  }
+
+  handleResize(e) {
+
+  }
 
   componentDidUpdate(prevProps) {
     const { defaultValue, test } = this.props;
@@ -189,28 +194,15 @@ export default class NumericInput extends React.Component {
         title={errMsg}
         visible={errMsg.length > 0}
       >
-        <div className={
-          []
-            .concat(this.className)
-            .concat("numeric-input-wrap")
-            .join(" ")
-            .trim()
-        }>
+        <div className={clsx(this.className, "numeric-input-wrap")}>
           
           <Input
             type="text"
-            inputMode={'decimal'}
+            inputMode="decimal"
             placeholder={0}
             maxLength={25}
             {...safeProps}
-            className={
-              []
-                .concat("numeric-input")
-                .concat(errMsg.length ? " error" : "")
-                .concat(isIPhone ? "iPhone" : "")
-                .join(" ")
-                .trim()
-            }
+            className={clsx("numeric-input", errMsg.length ? " error" : "", isMobile ? "iPhone" : "")}
             onKeyDown={e => this.onKeyDown(e)}
             onChange={e => this.onChange(e)}
             onFocus={e => this.onFocus(e)}
@@ -218,10 +210,9 @@ export default class NumericInput extends React.Component {
             value={value}
           />
 
-          {!unsigned && isIPhone && (
+          {!unsigned && isMobile && (
             <button
-              // className={"iPhone-button"}
-              onClick={(e) => {
+              onClick={e => {
                 const parsedValue = +(String(value).replace(/\s+/g, ""));
                 if (parsedValue !== 0) {
                   this.setState({
@@ -235,7 +226,13 @@ export default class NumericInput extends React.Component {
                 }
               }}
             > 
-              <svg className={positive ? "" : "negative"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 447.243 447.243"><path d="M420.361 192.229a31.967 31.967 0 00-5.535-.41H99.305l6.88-3.2a63.998 63.998 0 0018.08-12.8l88.48-88.48c11.653-11.124 13.611-29.019 4.64-42.4-10.441-14.259-30.464-17.355-44.724-6.914a32.018 32.018 0 00-3.276 2.754l-160 160c-12.504 12.49-12.515 32.751-.025 45.255l.025.025 160 160c12.514 12.479 32.775 12.451 45.255-.063a32.084 32.084 0 002.745-3.137c8.971-13.381 7.013-31.276-4.64-42.4l-88.32-88.64a64.002 64.002 0 00-16-11.68l-9.6-4.32h314.24c16.347.607 30.689-10.812 33.76-26.88 2.829-17.445-9.019-33.88-26.464-36.71z"/></svg>
+              <svg
+                className={positive ? "" : "negative"}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 447.243 447.243">
+                  <path d="M420.361 192.229a31.967 31.967 0 00-5.535-.41H99.305l6.88-3.2a63.998 63.998 0 0018.08-12.8l88.48-88.48c11.653-11.124 13.611-29.019 4.64-42.4-10.441-14.259-30.464-17.355-44.724-6.914a32.018 32.018 0 00-3.276 2.754l-160 160c-12.504 12.49-12.515 32.751-.025 45.255l.025.025 160 160c12.514 12.479 32.775 12.451 45.255-.063a32.084 32.084 0 002.745-3.137c8.971-13.381 7.013-31.276-4.64-42.4l-88.32-88.64a64.002 64.002 0 00-16-11.68l-9.6-4.32h314.24c16.347.607 30.689-10.812 33.76-26.88 2.829-17.445-9.019-33.88-26.464-36.71z"
+              />
+              </svg>
             </button>
           )}
 
