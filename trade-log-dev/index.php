0baee8207faa9b50c82dd126e97c0f606d3d072d
Metarenessans <zero.neitrino@gmail.com>
2021-10-25 16:43:26 +0300

    wepack fix

--- trade-log-dev/index.php
+++ trade-log-dev/index.php
@@ -37,8 +37,7 @@ $APPLICATION->SetTitle("Журнал сделок");
   <noscript>You need to enable JavaScript to run this app</noscript>
   <div id="root"></div>
   
-  <script type="module" src="public/index-es6.js"></script>
-  <script nomodule src="public/index.js"></script>
+  <script src="public/js/index.js"></script>
 
   <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
 </body>
--- trade-log-dev/webpack.config.js
+++ trade-log-dev/webpack.config.js
@@ -1,72 +1,32 @@
-const path = require("path");
 const webpack = require("webpack");
-/* Plugins */
+const path = require("path");
+// Plugins
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = (env, options) => {
   const prod = options.mode === "production";
 
-  const entry = "./src/js/index.js";
-  const devtool = prod ? "source-map" : "eval-source-map";
-  const publicPath = "public";
-
-  // Rules
-
-  const cssPipeline = [
-    // Translates CSS into CommonJS
-    "css-loader?url=false",
-    // PostCSS
-    {
-      loader: "postcss-loader",
-      options: {
-        plugins: [
-          require("postcss-custom-properties")(),
-          require("autoprefixer")({
-            overrideBrowserslist: ["ie >= 8", "last 4 version"]
-          }),
-          require("postcss-csso"),
-        ],
-        sourceMap: true
-      }
-    },
-    "resolve-url-loader",
-    // Compiles Sass to CSS
-    {
-      loader: "sass-loader",
-      options: {
-        prependData: `$fonts: '../${prod ? "" : "public/"}fonts/';`,
-        webpackImporter: false,
-        sassOptions: {
-          publicPath: "./",
-          outputStyle: "expanded",
-        },
-      },
+  const fileLoaderOptions = {
+    loader: "file-loader",
+    query: {
+      name: "[path][name].[ext]"
     }
-  ];
-
-  const plugins = [
-    new webpack.DefinePlugin({ dev: !prod }),
-    new MiniCssExtractPlugin({
-      filename:      "css/style.css",
-      chunkFilename: "css/[name].css"
-    })
-  ];
+  }
 
   return {
-    entry,
-    devtool,
-    devServer: {
-      contentBase: path.join(__dirname, ""),
-      overlay: true,
-      hot: true,
-      // host: '192.168.0.129'
-    },
+    entry: "./src/js/index.js",
     output: {
-      path: path.resolve(__dirname, publicPath),
-      filename: `js/index.js`,
-      chunkFilename: 'js/[name].js',
-      publicPath: publicPath + "/"
+      path: path.resolve(__dirname, "public"),
+      filename: "js/index.js"
     },
+    devServer: {
+      contentBase: "./public",
+      publicPath: "/public/js/",
+      overlay: true
+    },
+    devtool: prod ? "source-map" : "eval-sourcemap",
+    target: "web",
     module: {
       rules: [
         {
@@ -82,26 +42,58 @@ module.exports = (env, options) => {
         },
         {
           test: /\.s[ac]ss$/i,
-          use: [prod ? MiniCssExtractPlugin.loader : "style-loader"].concat(cssPipeline)
+          use: [
+            process.env.NODE_ENV !== "production"
+              ? "style-loader"
+              : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            "css-loader?url=false",
+            // PostCSS
+            {
+              loader: "postcss-loader",
+              options: {
+                plugins: [
+                  require("postcss-custom-properties")(),
+                  require("autoprefixer")({
+                    overrideBrowserslist: ["ie >= 8", "last 4 version"]
+                  }),
+                  require("postcss-csso"),
+                ],
+                sourceMap: true
+              }
+            },
+            "resolve-url-loader",
+            // Compiles Sass to CSS
+            {
+              loader: "sass-loader",
+              options: {
+                prependData: `$fonts: '../${prod ? "" : "public/"}fonts/';`,
+                webpackImporter: false,
+                sassOptions: {
+                  publicPath: "./",
+                  outputStyle: "expanded",
+                },
+              },
+            }
+          ]
         },
-        // Fonts
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          loader: "file-loader",
-          options: {
-            name: "[path][name].[ext]"
-          }
+          ...fileLoaderOptions
         },
-        // Graphics
         {
           test: /\.(png|jpe?g|gif|webp)$/,
-          loader: "file-loader",
-          options: {
-            name: "[path][name].[ext]"
-          }
+          ...fileLoaderOptions
         }
       ]
     },
-    plugins,
-  };
+    plugins: [
+      new webpack.DefinePlugin({ dev: !prod }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[name].css",
+      }),
+      new CleanWebpackPlugin(["public/js/*"])
+    ]
+  }
 };
