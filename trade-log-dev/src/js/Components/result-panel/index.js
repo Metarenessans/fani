96ed8fe2423591ffcd80cb9596396108ca87ba08
persiklover <dannniska@gmail.com>
2021-11-12 16:48:34 +0300

    Добавляет формулу для расчета рискометра

--- trade-log-dev/src/js/Components/result-panel/index.js
+++ trade-log-dev/src/js/Components/result-panel/index.js
@@ -25,6 +25,22 @@ export default function ResultPanel() {
   // Если включен лимит убыточных сделок и мы превышаем допустимый лимит - запрещаем торговлю
   let tradingNotAllowed = state.limitUnprofitableDeals && unprofitableDeals.length > state.allowedNumberOfUnprofitableDeals;
 
+  // TODO: Расчитать `percent` без использования `negativePoints`
+  let positivePoints = 0;
+  let negativePoints = 0;
+  for (let deal of currentDay.deals) {
+    positivePoints += deal.emotionalStates.positive.filter(value => value === true).length;
+    negativePoints += deal.emotionalStates.negative.filter(value => value === true).length;
+    positivePoints += deal.motives.positive.filter(value => value === true).length;
+    negativePoints += deal.motives.negative.filter(value => value === true).length;
+  }
+  let total = positivePoints + negativePoints;
+
+  let percent = ~~(positivePoints / total * 100);
+  if (total === 0) {
+    percent = 50;
+  }
+
   return (
     <Panel 
       // title="Мониторинг раппорта" 
@@ -32,11 +48,13 @@ export default function ResultPanel() {
       contentClassName="result-panel-content"
     >
       <Stack space="1em">
-        <Riskometer value={50} />
+        <Riskometer value={percent} />
         {/* TODO: Добавить aria-label */}
-        {/* <Button className="custom-btn result-panel-btn" disabled>
-          НИМБА
-        </Button> */}
+        {false && 
+          <Button className="custom-btn result-panel-btn" disabled>
+            НИМБА
+          </Button>
+        }
       </Stack>
       <strong className={clsx(
         "result-panel__action",
