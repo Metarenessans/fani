052fc34427270e15d01e96010815f3640682bcaa
persiklover <dannniska@gmail.com>
2021-11-14 15:33:35 +0300

    Добавляет документацию + рефактор

--- common/api/fetch/investor-info/index.js
+++ common/api/fetch/investor-info/index.js
@@ -1,3 +1,21 @@
+/**
+ * Шаблон ответа с сервера при получении профиля инвестора 
+ * 
+ * @typedef InvestorInfoResponse 
+ * @property {boolean} error
+ * @property {InvestorInfo} data
+ */
+
+/**
+ * Профиль инвестора
+ * 
+ * @typedef InvestorInfo
+ * @property {string} email Email
+ * @property {number} deposit Депозит
+ * @property {"KSUR"|"KPUR"} status
+ * @property {"UNSKILLED"|"SKILLED"} skill
+ */
+
 import { fetchInvestorInfo } from "./fetch-investor-info"
 import { applyInvestorInfo } from "./apply-investor-info"
 export { fetchInvestorInfo, applyInvestorInfo }
