385e707c7bc0570e2379dbe16148c953564787ad
persiklover <dannniska@gmail.com>
2021-11-14 15:10:20 +0300

    Добавляет документацию

--- common/utils/format-number.js
+++ common/utils/format-number.js
@@ -1,4 +1,10 @@
-export default function formatNumber(number = 0) {
+/**
+ * Форматирует число `number`, расставляя пробелы между разрядами
+ *
+ * @param {number} number Число, которое нужно отформатировать
+ * @returns {string}
+ */
+export default function formatNumber(number) {
   var chunks = String(number).split(".");
   var result = chunks[0].replace(/(\d)(?=(\d{3})+([^\d]|$))/g, "$1 ");
   if (chunks[1]) {
