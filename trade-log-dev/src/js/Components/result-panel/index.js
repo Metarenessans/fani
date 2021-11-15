792cf9c5e13514e71c70439e55d4a81dafc9acc9
persiklover <dannniska@gmail.com>
2021-11-15 23:38:33 +0300

    Прикручивает parseEmotionalState

--- trade-log-dev/src/js/Components/result-panel/index.js
+++ trade-log-dev/src/js/Components/result-panel/index.js
@@ -4,6 +4,8 @@ import clsx from "clsx"
 
 import Stack from "../../../../../common/components/stack"
 
+import parseEmotionalState from "../../utils/parse-emotional-state"
+
 import Panel      from "../panel"
 import Riskometer from "../riskometer"
 
@@ -16,31 +18,23 @@ export default function ResultPanel() {
   const { state } = context;
   const { data, currentRowIndex } = state;
   const currentDay = data[currentRowIndex];
+  const { deals } = currentDay;
 
   // 2 убыточные сделки подряд = нужно сделать перерыв
-  const shouldTakeBreak = currentDay.deals.some((deal, index, arr) => deal.result < 0 && arr[index + 1]?.result < 0);
+  const shouldTakeBreak = deals.some((deal, index, arr) => deal.result < 0 && arr[index + 1]?.result < 0);
   
   // Убыточные сделки
-  const unprofitableDeals = currentDay.deals.filter(deal => deal.result < 0);
+  const unprofitableDeals = deals.filter(deal => deal.result < 0);
   // Если включен лимит убыточных сделок и мы превышаем допустимый лимит - запрещаем торговлю
   let tradingNotAllowed = state.limitUnprofitableDeals && unprofitableDeals.length >= state.allowedNumberOfUnprofitableDeals;
 
-  // TODO: Расчитать `percent` без использования `negativePoints`
-  let positivePoints = 0;
-  let negativePoints = 0;
-  for (let deal of currentDay.deals) {
-    positivePoints += deal.emotionalStates.positive.filter(value => value === true).length;
-    negativePoints += deal.emotionalStates.negative.filter(value => value === true).length;
-    positivePoints += deal.motives.positive.filter(value => value === true).length;
-    negativePoints += deal.motives.negative.filter(value => value === true).length;
-  }
-  let total = positivePoints + negativePoints;
-
-  let percent = ~~(positivePoints / total * 100);
+  const { total, positive } = parseEmotionalState(deals);
+  let percent = ~~(positive / total * 100);
   if (total === 0) {
     percent = 50;
   }
 
+  // Запрещает торговю, если эмоциональный фон вышел в негатив 
   if (percent < 50) {
     tradingNotAllowed = true;
   }
