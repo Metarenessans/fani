ac571da97ff68ecd0ebd998657f8c73b1d6432af
persiklover <dannniska@gmail.com>
2021-11-16 00:48:23 +0300

    Добавляет документацию и небольшой рефактор

--- common/utils/cropp-number.js
+++ common/utils/cropp-number.js
@@ -1,3 +1,10 @@
+/**
+ * Обрезает число `number` до `precision` знаков после запятой без округления
+ *
+ * @param {number} number
+ * @param {number} precision
+ */
 export default function croppNumber(number, precision) {
-  return Math.trunc(number * Math.pow(10, precision)) / Math.pow(10, precision);
+  const powerOfTen = 10 ** precision;
+  return Math.trunc(number * powerOfTen) / powerOfTen;
 }
