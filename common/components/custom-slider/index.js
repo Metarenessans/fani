3e96765f558312abee1b0d3994da505dcce9416c
persiklover <dannniska@gmail.com>
2021-11-16 00:48:03 +0300

    Добавляет документацию и propTypes

--- common/components/custom-slider/index.js
+++ common/components/custom-slider/index.js
@@ -1,16 +1,109 @@
-import React from 'react'
-import { Slider, Tooltip } from 'antd/es'
-import clsx from 'clsx'
-
+import React from "react"
+import PropTypes from "prop-types"
+import { Slider, Tooltip } from "antd"
+import clsx from "clsx"
 import croppNumber from "../../utils/cropp-number"
 
 import "./style.scss"
 
+const propTypes = {
+  /**
+   * Значение ползунка
+   * 
+   * @type {number}
+   */
+  value: PropTypes.number,
+
+  /**
+   * Шаг
+   * 
+   * @type {number}
+   */
+  step: PropTypes.number,
+
+  /**
+   * Если включен, то ползунок будет задавать диапазон
+   * 
+   * @type {boolean}
+   */
+  range: PropTypes.bool,
+
+  /**
+   * Минимальное значение
+   * 
+   * @type {number}
+   */
+  min: PropTypes.number,
+
+  /**
+   * Максимальное значение
+   * 
+   * @type {number}
+   */
+  max: PropTypes.number,
+
+  /**
+   * @type {boolean}
+   * @deprecated Use `withValue` instead
+   */
+  showValue: PropTypes.bool,
+
+  /**
+   * Если включен, то рядом со слайдером будет рендериться `value`
+   * 
+   * По дефолту включен
+   * 
+   * @type {boolean}
+   */
+  withValue: PropTypes.bool,
+
+  /** 
+   * @type {boolean}
+   * @deprecated Use `withTooltip` instead
+   */
+  tooltipVisible: PropTypes.bool,
+
+  /**
+   * Если включен, то при ручном изменении значения над ручкой ползунка будет рендериться {@link Tooltip}
+   * 
+   * По дефолту выключен
+   * 
+   * @type {boolean}
+   */
+  withTooltip: PropTypes.bool,
+
+  /**
+   * Коллбэк, который вызывается каждый раз при изменении значения
+   * 
+   * @type {(value: number) => any}}
+   */
+  onChange: PropTypes.func,
+
+  /**
+   * Коллбэк, который вызывается после того, как пользователь отпустил ручку ползунка
+   * 
+   * @type {(value: number) => any}}
+   */
+  onAfterChange: PropTypes.func,
+
+  /**
+   * Функция для форматирования выводимого значения
+   * 
+   * @type {(value: number) => any}}
+   */
+  filter: PropTypes.func,
+};
+
+/** @augments React.Component<propTypes> */
 export default class CustomSlider extends React.Component {
+
+  static propTypes = propTypes;
+
+  /** @param {propTypes} props */
   constructor(props) {
     super(props);
 
-    const { value, filter, range } = this.props;
+    const { value, filter, range } = props;
 
     if (range) {
       this.range = true;
@@ -33,28 +126,37 @@ export default class CustomSlider extends React.Component {
   }
 
   render() {
-    var { value, precision, className, showValue, tooltipVisible, onAfterChange } = this.props;
-    tooltipVisible = tooltipVisible;
-    precision      = precision || 0; 
-    showValue      = showValue || false;
-
+    let {
+      value,
+      step,
+      precision,
+      className,
+      withValue,
+      withTooltip,
+      tooltipVisible,
+      onAfterChange
+    } = this.props;
+    step = Number.isFinite(step) ? step : 1;
+    precision = precision ?? 0;
+    withValue = withValue ?? true;
+    withTooltip = withTooltip ?? tooltipVisible ?? false;
     return (
-      <div className={clsx("custom-slider", className)}>
+      <div className={clsx("custom-slider", withValue && "with-value", className)}>
         <Slider
-          { ...this.props }
+          {...this.props}
           className="custom-slider__input"
-          tooltipVisible={tooltipVisible}
+          tooltipVisible={withTooltip}
           value={value}
-          onChange={e => this.onChange(e)}
-          onAfterChange={e => onAfterChange && onAfterChange(e)}
+          onChange={value => this.onChange(value)}
+          onAfterChange={value => onAfterChange && onAfterChange(value)}
         />
-        {showValue && (
+        {withValue &&
           <Tooltip title={this.filter( croppNumber(value, 7) )}>
             <span className="custom-slider__value">
               { this.filter( croppNumber(value, precision) ) }
             </span>
           </Tooltip>
-        )}
+        }
       </div>
     )
   }
