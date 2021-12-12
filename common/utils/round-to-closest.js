b4eb444dee62f58aa1d2575c026b946204487a77
persiklover <dannniska@gmail.com>
2021-12-12 21:19:37 +0300

    Добавляет TODO

--- common/utils/round-to-closest.js
+++ common/utils/round-to-closest.js
@@ -1,6 +1,10 @@
-/** Примагничивает число к ближайшему шагу
+// TODO: удалить, `magnetToClosest` делает то же самое
+/** 
+ * Примагничивает число к ближайшему шагу
+ * 
  * @param {number} value Число, которое нужно примагнитить
  * @param {number} step Число, к которому нужно примагнитить
+ * @todo Удалить, `magnetToClosest` делает то же самое
  */
 export default function roundToClosest(value, step) {
   return Math.floor(value / step) * step;
