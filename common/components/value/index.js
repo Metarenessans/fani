2909cd538e3d8c16e9d31cda3271d01ea05a78b5
persiklover <dannniska@gmail.com>
2021-12-23 01:31:10 +0300

    Добавляет пропс `forcedPlus`

--- common/components/value/index.js
+++ common/components/value/index.js
@@ -24,17 +24,31 @@ import "./style.scss"
  * `value > 0   -> "success"`
  * 
  * @param {(value: any) => any} props.format Коллбэк, в который передается `value` для форматирования
+ * @param {boolean} props.forcedPlus Добавляет к отформатированному положительному значению `+`,
+ * если равен `true`
  * @param {string} props.className
  */
 export default function Value({
   value,
   type,
   format,
+  forcedPlus,
   className,
   children
 }) {
   value = value ?? children;
   type = type ?? (value === 0 ? "default" : value < 0 ? "danger" : "success");
-  format = format ?? (typeof value === "number" ? formatNumber : null);
+  format = format ?? function(value) {
+    let formatted = value;
+    if (typeof value === "number") {
+      formatted = formatNumber(value);
+    }
+
+    if (forcedPlus && value > 0) {
+      formatted = "+" + formatted;
+    }
+
+    return formatted;
+  };
   return <span className={clsx("value", type, className)}>{format?.(value) ?? value}</span>
 }
