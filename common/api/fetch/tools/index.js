c83d7b9ced5208790a3cd5bde848a4891811325f
persiklover <dannniska@gmail.com>
2022-01-06 16:30:10 +0300

    small fix

--- common/api/fetch/tools/index.js
+++ common/api/fetch/tools/index.js
@@ -1,4 +1,3 @@
 import fetchTools from "./fetch-tools";
-import fetchTool  from "./fetch-tool";
 import applyTools from "./apply-tools";
-export { fetchTools, applyTools, fetchTool };
+export { fetchTools, applyTools };
