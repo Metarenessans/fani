a5137764b2369fc36f5d146254848e04e76bb377
Metarenessans <zero.neitrino@gmail.com>
2022-01-11 19:24:19 +0300

    Новый функционал с (выполнено), сортировка, обратная совместимость

--- trade-log-dev/src/js/Components/step-4/index.js
+++ trade-log-dev/src/js/Components/step-4/index.js
@@ -3,6 +3,8 @@ import { StateContext } from "../../App";
 import { cloneDeep } from "lodash";
 import { Button, Checkbox, Input } from "antd/es";
 
+import parseTasks from "../stats/tasks/parse-tasks";
+
 import "./style.scss";
 
 export default class FourthStep extends React.Component {
@@ -22,6 +24,7 @@ export default class FourthStep extends React.Component {
             technology,
             customTechnology,
             practiceWorkTasks,
+            readyWorkTasks,
             customPracticeWorkTasks
           } = data[currentRowIndex];
 
@@ -154,8 +157,15 @@ export default class FourthStep extends React.Component {
                 <div className="fourth-step-column">
                   <div className="fourth-step-table">
                     {/* TODO: should be h1-h6 */}
-                    <div className="fourth-step-table-title">
-                      Практические задачи на отработку
+                    <div className="fourth-step-table-title fourth-step-table-title--practice">
+                      <p className="fourth-step-table-title--practice-title">
+                        Практические задачи на отработку  
+                      </p>
+                      <div className="fourth-step-table-title--ready">
+                        <p>
+                          Готово
+                        </p>
+                      </div>
                     </div>
                     
                     <div className="fourth-step-table-row-container">
@@ -163,18 +173,42 @@ export default class FourthStep extends React.Component {
                         return (
                           <div className="fourth-step-table-row-container-row" key={index}>
                             <p>{taskName}</p>
-                            <div className="fourth-step-table-check-box">
-                              <Checkbox
-                                key={currentRowIndex}
-                                className="green"
-                                checked={practiceWorkTasks[taskName]}
-                                onChange={e => {
-                                  const { checked } = e.target;
-                                  const data = cloneDeep(state.data);
-                                  data[currentRowIndex].practiceWorkTasks[taskName] = checked;
-                                  context.setState({ data });
-                                }}
-                              />
+                            <div className="fourth-step-table-check-box fourth-step-table-check-box--practice">
+                              <div className="fourth-step-table-check-box fourth-step-table-check-box--practice-container">
+                                <Checkbox
+                                  key={currentRowIndex}
+                                  className="green" 
+                                  checked={practiceWorkTasks[taskName]}
+                                  onChange={e => {
+                                    const { checked } = e.target;
+                                    const data = cloneDeep(state.data);
+                                    data[currentRowIndex].practiceWorkTasks[taskName] = checked;
+                                    context.setState({ data });
+                                  }}
+                                />
+                              </div>
+
+                              <div className="fourth-step-table-check-box fourth-step-table-check-box--practice-container">
+                                <Checkbox
+                                  key={currentRowIndex}
+                                  className="green"
+                                  checked={readyWorkTasks[taskName]}
+                                  onChange={e => {
+                                    const readyWorkTasksCheckTime = cloneDeep(state.readyWorkTasksCheckTime);
+
+                                    const { checked } = e.target;
+                                    const data = cloneDeep(state.data);
+                                    data[currentRowIndex].readyWorkTasks[taskName] = checked;
+
+                                    let tasks = parseTasks(data);
+                                    if (tasks[taskName].done == tasks[taskName].checked) {
+                                      readyWorkTasksCheckTime[taskName] = Number(new Date());
+                                    }
+
+                                    context.setState({ data, readyWorkTasksCheckTime });
+                                  }}
+                                />
+                              </div>
                             </div>
                           </div>
                         );
@@ -193,33 +227,60 @@ export default class FourthStep extends React.Component {
                               }
                               onBlur={e => {
                                 const { value } = e.target;
-                                const data = cloneDeep(state.data);
                                 let i = data[currentRowIndex].customPracticeWorkTasks.findIndex(task => task.name == name);
                                 if (i === -1) {
                                   console.warn(`Не удлось найти '${name}' среди`, data[currentRowIndex].customPracticeWorkTasks);
                                   i = index;
                                 }
+                                const oldName = data[currentRowIndex].customPracticeWorkTasks[i].name;
                                 data[currentRowIndex].customPracticeWorkTasks[i].name = value;
+
+                                // Меняем ключ в массиве готовых задачу
+                                for (let day of data) {
+                                  const customTask = day.customPracticeWorkTasks.find(task => task.name == oldName);
+                                  if (customTask) {
+                                    customTask.name = value;
+                                  }
+
+                                  const checked = day.readyCustomPracticeWorkTasks[oldName];
+                                  delete day.readyCustomPracticeWorkTasks[oldName];
+                                  day.readyCustomPracticeWorkTasks[value] = checked;
+                                }
+
                                 context.setState({ data });
                               }}
                             />
-                            <div className="fourth-step-table-check-box">
-                              <Checkbox
-                                key={currentRowIndex}
-                                className="green"
-                                checked={value}
-                                onChange={e => {
-                                  const { checked } = e.target;
-                                  const data = cloneDeep(state.data);
-                                  let i = data[currentRowIndex].customPracticeWorkTasks.findIndex(task => task.name == name);
-                                  if (i === -1) {
-                                    console.warn(`Не удлось найти '${name}' среди`, data[currentRowIndex].customPracticeWorkTasks);
-                                    i = index; 
-                                  }
-                                  data[currentRowIndex].customPracticeWorkTasks[i].value = checked;
-                                  context.setState({ data });
-                                }}
-                              />
+                            <div className="fourth-step-table-check-box fourth-step-table-check-box--practice">
+                              <div className="fourth-step-table-check-box fourth-step-table-check-box--practice-container">
+                                <Checkbox
+                                  key={currentRowIndex}
+                                  className="green"
+                                  checked={value}
+                                  onChange={e => {
+                                    const { checked } = e.target;
+                                    let i = data[currentRowIndex].customPracticeWorkTasks.findIndex(task => task.name == name);
+                                    if (i === -1) {
+                                      console.warn(`Не удлось найти '${name}' среди`, data[currentRowIndex].customPracticeWorkTasks);
+                                      i = index;
+                                    }
+                                    data[currentRowIndex].customPracticeWorkTasks[i].value = checked;
+                                    context.setState({ data });
+                                  }}
+                                />
+                              </div>
+
+                              <div className="fourth-step-table-check-box fourth-step-table-check-box--practice-container">
+                                <Checkbox
+                                  key={currentRowIndex}
+                                  className="green"
+                                  checked={data[currentRowIndex].readyCustomPracticeWorkTasks[name]}
+                                  onChange={e => {
+                                    const { checked } = e.target;
+                                    data[currentRowIndex].readyCustomPracticeWorkTasks[name] = checked;
+                                    context.setState({ data });
+                                  }}
+                                />
+                              </div>
                             </div>
                           </div>
                         );
@@ -230,14 +291,15 @@ export default class FourthStep extends React.Component {
                     <Button
                       className="trade-log-button"
                       onClick={() => {
-                        const data = cloneDeep(state.data);
-                        const customPracticeWorkTasks = cloneDeep(data[currentRowIndex].customPracticeWorkTasks);
-                        customPracticeWorkTasks.push({
-                          name:  "Новая задача " +  (customPracticeWorkTasks.length + 1),
-                          value: false
+                        const cloneData = cloneDeep(data);
+
+                        cloneData.map((item, index) => {
+                          cloneData[index].customPracticeWorkTasks.push({
+                            name: "Новая задача " + (customPracticeWorkTasks.length + 1),
+                            value: false
+                          });
                         });
-                        data[currentRowIndex].customPracticeWorkTasks = customPracticeWorkTasks;
-                        context.setState({ data });
+                        context.setState({ data: cloneData });
                       }}
                     >
                       Добавить задачу
@@ -248,10 +310,16 @@ export default class FourthStep extends React.Component {
                       disabled={customPracticeWorkTasks.length === 0}
                       onClick={() => {
                         const data = cloneDeep(state.data);
-                        const customPracticeWorkTasks = cloneDeep(data[currentRowIndex].customPracticeWorkTasks);
-                        customPracticeWorkTasks.splice(customPracticeWorkTasks.length - 1, 1);
-                        data[currentRowIndex].customPracticeWorkTasks = customPracticeWorkTasks;
-                        context.setState({ data });
+                        const certainCustomPracticeWorkTasks = cloneDeep(data[currentRowIndex].customPracticeWorkTasks);
+                        const name = cloneDeep(data[currentRowIndex].customPracticeWorkTasks[certainCustomPracticeWorkTasks.length - 1].name);
+                        
+
+                        let newData = data.map(day => {
+                          day.customPracticeWorkTasks = day.customPracticeWorkTasks.filter(item => item.name !== name);
+                          return day;
+                        });
+
+                        context.setState({ data: newData });
                       }}
                     >
                       Удалить задачу
